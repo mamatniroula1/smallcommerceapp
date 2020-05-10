70dca8a0336a15ee61e35e795c55c487734dbc19
mamat <mamatniroula1@gmail.com>
2020-05-10 02:28:36 -0500

    Merge branch 'master' of https://github.com/mamatniroula1/smallcommerceapp  merged the earlier branches .

---
+++ src/main/java/com/sunil/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.sunil.ecommerce.dao;
+
+import com.sunil.ecommerce.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
